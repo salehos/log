f25f143694db2c94bd0b7619387ed09742da822f
salehos763 <salehos763@gmail.com>
2021-05-22 13:17:22 +0430

    Add all files to project

---
+++ mslog/log.py
@@ -0,0 +1,65 @@
+import logging
+import threading
+import json
+from kafka import KafkaProducer
+import traceback as tb
+
+
+def create_json_message(message, log_level, module_name, trace_back):
+    return {
+        'message': message,
+        'log_level': log_level,
+        'module_name': module_name,
+        'trace_back': trace_back,
+    }
+
+
+def json_serializer(data):
+    return json.dumps(data).encode('utf-8')
+
+
+def send_to_kafka(kafka_servers, json_message, kafka_topic):
+    producer = KafkaProducer(bootstrap_servers=kafka_servers,
+                             value_serializer=json_serializer)
+    producer.send(kafka_topic, json_message)
+
+
+def create_kafka_thread(kafka_servers, json_message, kafka_topic):
+    y = threading.Thread(target=send_to_kafka, args=(kafka_servers, json_message, kafka_topic))
+    y.start()
+
+
+class Log:
+    def __init__(self, client_id, kafka_servers, module_name, kafka_logging):
+        self.client_id = client_id
+        self.kafka_servers = kafka_servers
+        self.module_name = module_name
+        self.kafka_logging = kafka_logging
+        logging.getLogger("kafka").setLevel(logging.ERROR or logging.WARN or logging.WARNING)
+
+    def log(self, message, log_level="info", log_name="log", kafka_topic="log"):
+        x = threading.Thread(target=do_log, args=(message, log_level, log_name))
+        x.start()
+        if self.kafka_logging:
+            traceback = ""
+            if log_level == "error" or log_level == "critical":
+                traceback = tb.format_exc()
+            json_message = create_json_message(message=message, log_level=log_level, module_name=self.module_name,
+                                               trace_back=traceback)
+            create_kafka_thread(self.kafka_servers, json_message, kafka_topic)
+
+
+def do_log(message, log_level, filename):
+    logging.basicConfig(filename=f'{filename}.log', level=logging.DEBUG,
+                        format='%(asctime)s.%(msecs)03d %(levelname)s : %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    if log_level == "critical":
+        logging.critical(message, exc_info=True)
+    elif log_level == "info":
+        logging.info(message)
+    elif log_level == "debug":
+        logging.debug(message)
+    elif log_level == "warn":
+        logging.warning(message)
+    elif log_level == "error":
+        logging.error(message, exc_info=True)
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='mslog',
+    packages=find_packages(),
+    version='0.1.0',
+    description='My log Python library',
+    author='Mohammad saleh saeidi',
+    license='MIT',
+    install_requires=['kafka-python==2.0.2'],
+    url="https://github.com/salehos/log",
+
+)
